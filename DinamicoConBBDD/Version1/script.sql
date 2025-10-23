737163e3d7bd4707e6797ac2cb52b030c14b0833
JQA31 <josephjoelquispealvarez.guadalupe@alumnado.fundacionloyola.net>
2025-10-23 12:29:00 +0200

    Delete DinamicoConBBDD directory

--- DinamicoConBBDD/Version1/script.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE Prueba1;
-USE Prueba1;
-CREATE TABLE Paises (
-    id char(5) PRIMARY KEY,
-    nombre char(50) NOT NULL
-);
-
-INSERT INTO Paises
-VALUES ('PE','Peru'),('CO','Colombia'),('MX','Mexico'),('AR','Argentina'),('ES','España'),('CL','Chile');
